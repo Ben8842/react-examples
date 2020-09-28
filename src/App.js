f402294fd22a7f1c388ec253c91c898177bbacff
Ben8842 <z87e99b44r55a@gmail.com>
2020-09-28 13:38:44 -0400

    The first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="body">
+      <Header />
+      <Card
+        className="section"
+        img="./Capture1.PNG"
+        title="Tik Tak Toe"
+        description="Tik Tak Toe game coded in React"
+      />
+
+      <Card
+        className="section bg-grey"
+        img="./Capture3.PNG"
+        title="To Do"
+        description="To Do application coded in React"
+      />
+
+      <Card
+        className="section"
+        img="./Capture1.PNG"
+        title="Log in Button"
+        description="Log in button coded in React"
+      />
+      <ContactContainer />
     </div>
   );
 }
 
-export default App;
+const Header = () => {
+  return (
+    <div className="header">
+      <span className="header-title">React</span>
+      <br />
+      <span className="header-text">Find React examples here</span>
+    </div>
+  );
+};
+
+const Card = (props) => {
+  return (
+    <div className={props.className}>
+      <div className="small-div">
+        <i className={props.className}></i>
+        <img src={props.img} alt="" />
+      </div>
+
+      <div className="big-div">
+        <span className="div-title">{props.title}</span>
+        <br />
+        <span>{props.description}</span>
+      </div>
+    </div>
+  );
+};
+
+const ContactContainer = () => {
+  return (
+    <div className="contact-container bg-grey">
+      <div className="contact-form">
+        <div id="sect1">
+          <span>
+            <i className="fas fa-map-marker-alt"></i>
+            Benjamin Perkins
+          </span>
+          <span>
+            <i className="fas fa-mobile-alt"></i>
+            perkinsben@yahoo.com
+          </span>
+          <span>
+            <i className="far fa-envelope"></i>
+            https://github.com/BenWPerkins
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
